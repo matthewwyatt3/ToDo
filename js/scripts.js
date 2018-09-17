ab52c05e235ac67be70cc42b91a9aa85f34cea15
Matthew Wyatt <mattwyatt3@gmail.com>
2018-09-17 01:48:42 -0700

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,22 @@
-var openTasks = document.getElementsByTagName("LI");
-var i;
-for (i = o; i < openTasks.length; i++) {
-  var span = document.createElement("SPAN");
-  var text = document.createTextNode("\u00D7");
-  span.className = "close"
-  span.appendChild(txt);
-  openTasks[i].appendChild(span);
-}
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("delete");
+//   span.className = "delete";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
+//
+// var myNodelist = document.getElementsByTagName("LI");
+// var i;
+// for (i = 0; i < myNodelist.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("complete");
+//   span.className = "complete";
+//   span.appendChild(txt);
+//   myNodelist[i].appendChild(span);
+// }
 
 
 
@@ -22,10 +32,47 @@ function newTask() {
   }
   document.getElementById("taskInput").value = "";
 
+  var span = document.createElement("BUTTON");
+  var txt = document.createTextNode("");
+  span.className = "delete fas fa-times-circle";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  var span = document.createElement("BUTTON");
+  var txt = document.createTextNode("");
+  span.className = "complete fas fa-check-square";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  var span = document.createElement("BUTTON");
+  var txt = document.createTextNode("");
+  span.className = "edit fas fa-pencil-alt";
+  span.appendChild(txt);
+  li.appendChild(span);
+
   for ( i = 0; i < close.length; i++) {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
+}
+
+var close = document.getElementsByClassName("delete");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var complete = document.getElementsByClassName("complete");
+var i;
+for (i = 0; i < close.length; i++) {
+  complete[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+  document.getElementById("done").appendChild(li);
 }
