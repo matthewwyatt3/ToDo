02fd2af6798840dd8c169df88265c114e13c42fc
Matthew Wyatt <mattwyatt3@gmail.com>
2018-09-17 12:44:44 -0700

    third commit

--- js/scripts.js
+++ js/scripts.js
@@ -41,12 +41,7 @@ function newTask() {
   var span = document.createElement("BUTTON");
   var txt = document.createTextNode("");
   span.className = "complete fas fa-check-square";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  var span = document.createElement("BUTTON");
-  var txt = document.createTextNode("");
-  span.className = "edit fas fa-pencil-alt";
+  span.idName = "mydiv";
   span.appendChild(txt);
   li.appendChild(span);
 
@@ -54,6 +49,8 @@ function newTask() {
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
+
+
     }
   }
 }
@@ -75,4 +72,63 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
   document.getElementById("done").appendChild(li);
+}
+
+// function complete() {
+// toDo = document.getElementById("toDo");
+//
+// for (var i=0; i < toDo.getElementsByTagName("li").length; i++) {
+//     toDo.getElementsByTagName("li")[i].onclick = function() {
+//         transfer = this.cloneNode(true);
+//         done.appendChild(transfer);
+//         toDo.removeChild(this);
+//         return false;
+//     }
+//
+// }
+//
+//
+// }
+
+dragElement(document.getElementById("toDo"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    /* if present, the header is where you move the DIV from:*/
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    /* otherwise, move the DIV from anywhere inside the DIV:*/
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
